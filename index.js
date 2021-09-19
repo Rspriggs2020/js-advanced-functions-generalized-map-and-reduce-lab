d46b1a4b472c70e30153a77ea89fbb0dee42313e
ryliespriggs <rspriggs812@gmail.com>
2021-09-19 11:14:02 -0700

    commit tree 89f79cbc63a32b621a6cd770df4bc407c357262b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+function map(number, source) {
+    const array = []
+    for (let i = 0; i < number.length; i ++) {
+        const newArray = number[i];
+        array.push(source(newArray))
+    };
+    return array
+}
+
+function reduce(number, source, startingValue) {
+    let total;
+    if (startingValue) {
+        total = startingValue;
+        for (let i = 0; i < number.length; i ++){
+            total = source(number[i], total);
+        }
+        return total;
+    }   else {
+        total = number[0];
+        for (let i = 1; i < number.length; i++) {
+            total = source(number[i], total);
+        }
+        return total;
+    }
+}
